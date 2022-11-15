0250c3f293cab9a75973e5604c189ccb54b2c5a1
Boobielicious® <112516618+boobielicious@users.noreply.github.com>
2022-11-15 21:41:57 +0100

    refactor: improved header

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const RootLayout = ({ children }: Props): JSX.Element => {
     <html lang="en">
       <head />
       <body className="bg-neutral-50 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-50">
-        <Header />
+        <Header className="sticky top-0 z-10 mx-auto h-12 max-w-screen-xl bg-neutral-50 px-4 dark:bg-neutral-900" />
         <main className="mx-auto max-w-screen-xl">{children}</main>
       </body>
     </html>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import Link from 'next/link'
-import { ChangeEvent, FormEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, HTMLAttributes, useState } from 'react'
 
-const Header = (): JSX.Element => {
+const Header = ({ className }: HTMLAttributes<HTMLDivElement>): JSX.Element => {
   const [searchValue, setSearchValue] = useState('')
 
   const handleSearchInputChange = (event: ChangeEvent<HTMLInputElement>): void => setSearchValue(event.target.value)
@@ -17,23 +17,25 @@ const Header = (): JSX.Element => {
     }
   }
   return (
-    <header className="sticky top-0 z-10 mx-auto flex h-12 max-w-screen-xl items-center justify-between bg-neutral-50 px-4 dark:bg-neutral-900">
-      <Link href="/" className="select-none text-2xl font-bold tracking-tighter">
-        <span className="sm:hidden">B</span>
-        <span className="hidden sm:block">Booblielicious&reg;</span>
-      </Link>
+    <header className={className}>
+      <div className="flex h-full items-center justify-between">
+        <Link href="/" className="select-none text-2xl font-bold tracking-tighter">
+          <span className="sm:hidden">B</span>
+          <span className="hidden sm:block">Booblielicious&reg;</span>
+        </Link>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          className="appearance-none rounded-none border-b border-neutral-300 bg-neutral-50 tracking-tight text-neutral-900 placeholder-neutral-300 outline-none dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-200 dark:placeholder-neutral-700"
-          type="search"
-          name="search"
-          placeholder="Search"
-          value={searchValue}
-          onChange={handleSearchInputChange}
-        />
-        <input type="submit" hidden />
-      </form>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="appearance-none rounded-none border-b border-neutral-300 bg-neutral-50 tracking-tight text-neutral-900 placeholder-neutral-300 outline-none dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-200 dark:placeholder-neutral-700"
+            type="search"
+            name="search"
+            placeholder="Search"
+            value={searchValue}
+            onChange={handleSearchInputChange}
+          />
+          <input type="submit" hidden />
+        </form>
+      </div>
     </header>
   )
 }
