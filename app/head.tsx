f442d962b4f5008659b327df226c583c00f74fd4
Boobielicious® <112516618+boobielicious@users.noreply.github.com>
2022-12-27 21:27:05 +0100

    chore: initial commit

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+const Head = (): JSX.Element => {
+  return (
+    <>
+      <title>Boobielicious&reg;</title>
+      <meta charSet="UTF-8" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+    </>
+  )
+}
+
+export default Head
