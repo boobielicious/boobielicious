f61a1588c44262a0862de940901a262acf7da1e3
Boobielicious® <112516618+boobielicious@users.noreply.github.com>
2023-01-05 19:51:28 +0100

    chore: removed file path from logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,5 +1,7 @@
 import { Logger } from 'tslog'
 
-const logger = new Logger()
+const logger = new Logger({
+  prettyLogTemplate: '{{yyyy}}.{{mm}}.{{dd}} {{hh}}:{{MM}}:{{ss}}:{{ms}}\t{{logLevelName}}\t[{{name}}]\t'
+})
 
 export default logger
